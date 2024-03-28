84f3edc41f567618a03ecc572f15f4100e26037e
Sergey <sk85post@gmail.com>
2024-03-28 21:33:09 +0300

    feat: complete getPolynom

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -92,8 +92,19 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  return function poly(x) {
+    switch (args.length) {
+      case 1:
+        return args[0];
+      case 2:
+        return args[0] * x + args[1];
+      case 3:
+        return args[0] * x ** 2 + args[1] * x + args[2];
+      default:
+        return null;
+    }
+  };
 }
 
 /**
